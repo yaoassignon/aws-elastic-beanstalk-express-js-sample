ad43e50c47c736706999b60cd86151bd3d89c19e
unknown <assignony@gmail.com>
2024-11-27 19:27:24 -0600

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('My name is Yao. DevOps Engineering is my passion'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
