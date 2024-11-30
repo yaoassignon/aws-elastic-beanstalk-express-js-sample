dd4229cb56f2888ee74fb82541fdbe65ea03b6c0
yaoassignon <assignony82@gmail.com>
2024-11-30 11:03:33 -0600

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My name is Yao. DevOps Engineering is my passion'));
+app.get('/', (req, res) => res.send('My name is Yao Assignon. DevOps Engineering is my passion'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
